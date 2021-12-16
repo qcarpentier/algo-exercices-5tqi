a2b08c93412b38d354bd12a02d22cd723d19c2c0
Quentin Carpentier <quentin.carpentier@outlook.be>
2021-12-16 09:09:45 +0100

    Solution de l'exercice 1

---
+++ exercice-1.py
@@ -0,0 +1,24 @@
+# Écrire un programme qui affiche les nombres de 1 à 100 en indiquant le départ, le milieu et l'arrivée.
+
+### Solution avec la boucle for ###
+
+print('Zé barti!')
+
+for number in range(1, 101):
+    print(number)
+
+    if number == 50: print('Zé la moitié!')
+    if number == 100: print('Zé derminé!')
+    
+
+### Autre solution avec la boucle While ###
+
+# number = 1
+
+# while number <= 100:
+#     print(number)
+
+#     if number == 50: print('Zé la moitié!')
+#     if number == 100: print('Zé derminé!')
+
+#     number += 1
